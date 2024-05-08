c4b63a9e7005bef0b2e5d3b9b09e3e10cb60b17a
lequo <lequockhanhkt03@gmail.com>
2024-05-08 16:42:00 +0700

    fix view profile member

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.navigation.ui)
     implementation(libs.legacy.support.v4)
     implementation(libs.activity)
+    implementation(libs.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
