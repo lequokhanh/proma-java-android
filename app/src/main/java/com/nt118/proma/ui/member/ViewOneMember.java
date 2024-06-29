e0b5c3b2f194bc87c1d13ffc6b58787e13e92123
lequo <lequockhanhkt03@gmail.com>
2024-06-30 01:47:05 +0700

    add view one member

--- app/src/main/java/com/nt118/proma/ui/member/ViewOneMember.java
+++ app/src/main/java/com/nt118/proma/ui/member/ViewOneMember.java
@@ -1,16 +1,18 @@
 package com.nt118.proma.ui.member;
 
+import static com.nt118.proma.ui.task.TaskDetail.setWindowFlag;
+
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.nt118.proma.R;
@@ -28,11 +30,10 @@ public class ViewOneMember extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_view_one_member);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
 
         Intent intent2 = getIntent();
         email = intent2.getStringExtra("email");
@@ -95,7 +96,7 @@ public class ViewOneMember extends AppCompatActivity {
                 tvEmail.setText(email);
                 tvPhone.setText(task.getResult().getDocuments().get(0).get("phone_number").toString());
                 if (task.getResult().getDocuments().get(0).get("avatar") != null) {
-                    imgAvatar.setBackgroundResource(new ImageArray().getAvatarImage().get(Math.toIntExact((Long) task.getResult().getDocuments().get(0).get("avatar"))));
+                    imgAvatar.setImageResource(new ImageArray().getAvatarImage().get(Math.toIntExact((Long) task.getResult().getDocuments().get(0).get("avatar"))));
                 }
             }
         });
